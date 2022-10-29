d38cef177378a72ebc645d138baab2ba2c716b47
barmalini18 <44473758+barmalini18@users.noreply.github.com>
2022-10-29 02:20:18 +0200

    Update command.sh

--- command.sh
+++ command.sh
@@ -1,6 +1,6 @@
 sh Miniconda3-py37_4.10.3-Linux-x86_64.sh
 export PATH=~/miniconda3/bin:$PATH
-git clone git@github.com:barmalini18/flask-ml-service.git
+git clone git@github.com:barmalini18/flask-ml-deploy.git
 cd flask-ml-service
 conda create -n env -f requirements.txt
 conda activate
